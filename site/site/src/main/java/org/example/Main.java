7ee8fe11128b5c649e9e4c289acaf87230f4fdf0
student <romanlabyak187@gmail.com>
2025-05-06 14:44:15 +0200

    6.05

--- site/site/src/main/java/org/example/Main.java
+++ site/site/src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import org.example.database.DatabaseConnection;
+
+import javax.xml.crypto.Data;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DatabaseConnection db = new DatabaseConnection();
+        db.connect("site.db");
+        try (Statement stmt = db.getConnection().createStatement()) {
+            stmt.executeUpdate("""     
+            CREATE TABLE IF NOT EXISTS test(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    value TEXT NOT NULL
+                    );
+            """);
+
+            stmt.executeUpdate("""
+                    DELETE FROM test;
+                    """);
+
+            stmt.executeUpdate("""
+                    INSERT INTO test (value) VALUES ('wartosc1'), ('wartosc2'),('wartosc3'), ('wartosc4');
+                    """);
+
+            ResultSet rs = stmt.executeQuery("""
+                    SELECT * FROM test;
+                    """);
+
+            while (rs.next()){
+                System.out.println(rs.getInt(1)+": "+ rs.getString("value"));
+            }
+
+        } catch (SQLException e){
+            System.err.println(e.getMessage());
+        }
+
+        db.disconnect();
+
     }
 }
--- site/site/src/main/java/org/example/database/DatabaseConnection.java
+++ site/site/src/main/java/org/example/database/DatabaseConnection.java
@@ -2,15 +2,30 @@ package org.example.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnection {
     private Connection connection;
 
     public void connect(String dbPath){
+        try{
         connection = DriverManager.getConnection("jdbc:sqlite:" + dbPath);
+        } catch (SQLException e){
+          throw new RuntimeException(e);
+        }
     }
-    public void disconnect(){
 
+    public void disconnect(){
+        if(connection==null){
+            return;
+        }
+
+        try {
+            connection.commit();
+            connection.close();
+        }catch (SQLException e){
+            System.err.println(e.getMessage());
+        }
     }
 
     public Connection getConnection() {
